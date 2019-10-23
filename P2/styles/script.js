f04f3c3416c7f10594a8ad84cb0fc64e1d2f5c3e
Mary Vainstein <vainsteinmary@cca.edu>
2019-10-22 18:39:59 -0700

    Create script.js

---
+++ P2/styles/script.js
@@ -0,0 +1,50 @@
+
+
+
+
+
+
+
+
+
+// document.addEventListener('mousemove',function(event){
+  
+
+// let t = document.querySelector('#t')
+// let t = 0
+
+// t.addEventListener('click',function(){
+//   if(tCount % 3 == 0){
+    
+
+
+//     console.log('zero')
+//     lx.style.fontSize = "150%"
+    
+//   } else if(t % 3 == 1){
+    
+    
+    
+//     console.log('one')
+//     lx.style.fontSize = "250%"
+    
+//   } else if(tCount % 3 == 2){
+    
+    
+    
+//     console.log('two')
+    
+//     lx.style.fontSize = "300%"
+    
+//     span.style.fontSize = "25px";
+//   }
+  
+//   tCount = tCount + 1
+  
+  
+//   console.log(tCount)
+  
+
+
+
+
